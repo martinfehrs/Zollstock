5c3ba0689ff5a8cfbc438fd69c401fb73da00810
Martin Fehrs <martinfehrs@web.de>
2023-12-21 10:34:45 +0100

    Einführung von Längeneinheitenanforderunen.

--- sources/length_units_requirements.cpp
+++ sources/length_units_requirements.cpp
@@ -1,8 +1,19 @@
 #include <zollstock/length_units.hpp>
 
+
 namespace zollstock
 {
 
-    static_assert()
+    static_assert(centimeter::exponents == unit_exponents{ 1       , 0      , 0     });
+    static_assert(centimeter::factors   == unit_factors  { 1e-2L   , 0.0L   , 0.0L  });
+    static_assert(centimeter::symbols   == unit_symbols  { "cm"_us , ""_us  , ""_us });
+
+    static_assert(decimeter ::exponents == unit_exponents{ 1       , 0      , 0     });
+    static_assert(decimeter ::factors   == unit_factors  { 1e-1L   , 0.0L   , 0.0L  });
+    static_assert(decimeter ::symbols   == unit_symbols  { "dm"_us , ""_us  , ""_us });
+
+    static_assert(meter     ::exponents == unit_exponents{ 1       , 0      , 0     });
+    static_assert(meter     ::factors   == unit_factors  { 1e0L    , 0.0L   , 0.0L  });
+    static_assert(meter     ::symbols   == unit_symbols  { "m"_us  , ""_us  , ""_us });
 
 }
