e8f7fce15852b6e2df46dc635b848c345a9fb895
Martin Fehrs <martinfehrs@web.de>
2024-01-04 11:36:27 +0100

    Verwendung des Begriffes symbol anstelle von suffix für den Makroparameter

--- include/zollstock/definition_helpers.hpp
+++ include/zollstock/definition_helpers.hpp
@@ -2,16 +2,17 @@
 #define __ZOLLSTOCK_DEFINITION_HELPERS_HPP__
 
 
-#define ZOLLSTOCK_DEFINE_LITERAL(type, suffix) \
-    [[nodiscard]] constexpr auto operator""_##suffix(unsigned long long int value) noexcept \
+#define ZOLLSTOCK_DEFINE_LITERAL(type, symbol) \
+    [[nodiscard]] constexpr auto operator""_##symbol(unsigned long long int value) noexcept \
     { \
         return scalar<type>{ static_cast<double>(value) }; \
     } \
     \
-    [[nodiscard]] constexpr auto operator""_##suffix(long double value) noexcept \
+    [[nodiscard]] constexpr auto operator""_##symbol(long double value) noexcept \
     { \
         return scalar<type>{ static_cast<double>(value) }; \
     } \
+
 
 
 #endif //__ZOLLSTOCK_DEFINITION_HELPERS_HPP__
