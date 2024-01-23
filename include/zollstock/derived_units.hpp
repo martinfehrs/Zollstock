380f96d41b2b52beba0689903b0ad02ae13eeac9
Martin Fehrs <martinfehrs@web.de>
2024-01-23 09:04:58 +0100

    Beseitigung von Einbindung des nicht mehr vorhandenen Headers velocity_units.hpp

--- include/zollstock/derived_units.hpp
+++ include/zollstock/derived_units.hpp
@@ -5,7 +5,6 @@
 #include <zollstock/angle_units.hpp>
 #include <zollstock/area_units.hpp>
 #include <zollstock/volume_units.hpp>
-#include <zollstock/velocity_units.hpp>
 
 
 #endif //__ZOLLSTOCK_DERIVED_UNITS_HPP__
