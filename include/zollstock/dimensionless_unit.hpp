3ae30db52d858d623731b0dc482855555cf9c1aa
Martin Fehrs <martinfehrs@web.de>
2024-01-26 13:30:57 +0100

    Umbenennung der Datei dimensionless_units.hpp in dimensionless_unit.hpp, da nur eine einzige dimensionslose Einheit existiert

---
+++ include/zollstock/dimensionless_unit.hpp
@@ -0,0 +1,10 @@
+#ifndef __ZOLLSTOCK_DIMENSIONLESS_UNITS_HPP__
+#define __ZOLLSTOCK_DIMENSIONLESS_UNITS_HPP__
+
+
+#include <zollstock/dimensionless_unit_types.hpp>
+#include <zollstock/dimensionless_unit_constants.hpp>
+#include <zollstock/dimensionless_unit_literals.hpp>
+
+
+#endif //__ZOLLSTOCK_DIMENSIONLESS_UNITS_HPP__
