8941db1faa354896e1fbff31499467e55d29c37a
Martin Fehrs <martinfehrs@web.de>
2024-01-26 13:29:09 +0100

    Korrektur der dimensionlosen Einheitenliterale

--- include/zollstock/dimensionless_unit_literals.hpp
+++ include/zollstock/dimensionless_unit_literals.hpp
@@ -2,15 +2,15 @@
 #define __ZOLLSTOCK_DIMENSIONLESS_UNIT_LITERALS_HPP__
 
 
-#include <zollstock/dimensionless_unit_types.hpp>
+#include <zollstock/dimensionless_unit_constants.hpp>
 #include <zollstock/definition_helpers.hpp>
-#include <zollstock/scalar.hpp>
+#include <zollstock/quantity.hpp>
 
 
 namespace zollstock::inline unit::inline literals
 {
 
-    ZOLLSTOCK_DEFINE_LITERAL(one, _1)
+    ZOLLSTOCK_DEFINE_LITERAL(_1)
 
 }
 
