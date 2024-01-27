5a13b657a80fc872dbb691390410707ac2450dd9
Martin Fehrs <martinfehrs@web.de>
2024-01-27 21:48:11 +0100

    Tests für Sonderfälle gemischter Größenvergleiche

--- tests/sources/quantity_tests.cpp
+++ tests/sources/quantity_tests.cpp
@@ -1,6 +1,6 @@
 #include <catch2/catch_all.hpp>
 #include <zollstock/quantity.hpp>
-#include <zollstock/dimensionless_unit_constants.hpp>
+#include <zollstock/dimensionless_unit.hpp>
 
 #include <climits>
 
@@ -11,10 +11,9 @@ namespace zs = zollstock;
 TEST_CASE("quantity", "[quantity]")
 {
 
-    // Kein Verlust des Vorzeichens bei Vergleichen
-    REQUIRE_FALSE(zs::int_t{ -1 } == zs::unsigned_t{ UINT_MAX });
-    REQUIRE(zs::int_t{ -1 } != zs::unsigned_t{ UINT_MAX });
-    REQUIRE(zs::int_t{ -1 } < zs::unsigned_t{ 1 });
-    REQUIRE(zs::unsigned_t{ 1 } > zs::int_t{ -1 });
+    REQUIRE_FALSE(int_t<>{ -1 } == unsigned_t<>{ UINT_MAX });
+    REQUIRE(int_t<>{ -1 } != unsigned_t<>{ UINT_MAX });
+    REQUIRE(int_t<>{ -1 } < unsigned_t<>{ 1 });
+    REQUIRE(unsigned_t<>{ 1 } > int_t<>{ -1 });
 
 }
