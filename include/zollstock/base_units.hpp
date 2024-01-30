28c3b81a7ea983a285919b1cdea4f678e63ad530
Martin Fehrs <martinfehrs@web.de>
2024-01-30 12:21:56 +0100

    Korrektur einer Header-Pfades

--- include/zollstock/base_units.hpp
+++ include/zollstock/base_units.hpp
@@ -2,7 +2,7 @@
 #define __ZOLLSTOCK_BASE_UNITS_HPP__
 
 
-#include <zollstock/dimensionless_units.hpp>
+#include <zollstock/dimensionless_unit.hpp>
 #include <zollstock/length_units.hpp>
 #include <zollstock/time_units.hpp>
 
