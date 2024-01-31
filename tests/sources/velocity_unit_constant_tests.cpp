73f2561419521730165bd1a3a99e065de7ab8a3e
Martin Fehrs <martinfehrs@web.de>
2024-01-31 15:54:57 +0100

    Geschwindigkeitskonstantentests

---
+++ tests/sources/velocity_unit_constant_tests.cpp
@@ -0,0 +1,16 @@
+#include <catch2/catch_all.hpp>
+#include <zollstock/length_unit_constants.hpp>
+#include <zollstock/time_unit_constants.hpp>
+#include <zollstock/unit_algorithms.hpp>
+#include <zollstock/tests/test_helpers.hpp>
+
+
+using namespace zollstock;
+
+
+TEST_CASE("velocity-unit-constants", "[velocity][unit][constants]")
+{
+
+    ZOLLSTOCK_TESTS_TEST_MIXED_DIVISION_UNIT_CONSTANT(m, s)
+
+}
