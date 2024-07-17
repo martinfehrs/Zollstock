c871ca710ec814453800e72f27f77bd07167a82f
martinfehrs <martinfehrs@web.de>
2024-07-17 21:39:01 +0200

    Korrektur der Einheit für Winkelkonstanten

--- include/zollstock/angle_unit_math_constants.hpp
+++ include/zollstock/angle_unit_math_constants.hpp
@@ -9,9 +9,9 @@
 namespace zollstock::inline math_constants
 {
 
-    inline constexpr scalar<radian, float      > pi_f_rad{ pi_f };
-    inline constexpr scalar<radian, double     > pi_rad  { pi   };
-    inline constexpr scalar<radian, long double> pi_l_rad{ pi_l };
+    inline constexpr scalar<rad, float      > pi_f_rad{ pi_f };
+    inline constexpr scalar<rad, double     > pi_rad  { pi   };
+    inline constexpr scalar<rad, long double> pi_l_rad{ pi_l };
 
 }
 
