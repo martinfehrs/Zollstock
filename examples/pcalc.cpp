6afbb1cb9dd6f257aa7eae836ff6edc9306f8f86
martinfehrs <martinfehrs@web.de>
2024-07-23 17:08:53 +0200

    Hinzufügen des Codebeispiels aus README.md als separates Beispielprojekt.

---
+++ examples/pcalc.cpp
@@ -0,0 +1,54 @@
+#include <zollstock/zollstock.hpp>
+#include <iostream>
+#include <cstdlib>
+
+namespace zs = zollstock;
+
+[[noreturn]] void argument_error() noexcept
+{
+    std::cerr << "Invalid arguments\n"
+              << "Usage: pcalc <wall thickness (cm)> <outer radius (cm)> <pipe length (m)>\n";
+
+    std::exit(1);
+}
+
+[[nodiscard]] auto read_args(int argc, char** argv) noexcept
+{
+    if (argc != 4)
+        argument_error();
+
+    try
+    {
+        return std::tuple{
+            zs::double_t<zs::cm>{ std::stod(argv[1]) },
+            zs::double_t<zs::cm>{ std::stod(argv[2]) },
+            zs::double_t<zs::cm>{ std::stod(argv[3]) },
+        };
+    }
+    catch(const std::exception& e)
+    {
+        argument_error();
+    }
+}
+
+int main(int argc, char** argv) try
+{
+    // Checking arguments
+    const auto [wall_thickness, outer_radius, pipe_length] = read_args(argc, argv);
+
+    // Calculation of a pipes material volume
+    const auto inner_radius = outer_radius - wall_thickness;
+    const auto outer_area = zs::pi * outer_radius * outer_radius;
+    const auto inner_area = zs::pi * inner_radius * inner_radius;
+    const auto outer_volume = outer_area * pipe_length;
+    const auto inner_volume = inner_area * pipe_length;
+    const zs::double_t<zs::dm3> pipe_volume = outer_volume - inner_volume;
+
+    std::cout << "pipe volume: " << pipe_volume << '\n';
+
+    return 0;
+}
+catch(std::exception&)
+{
+    argument_error();
+}
