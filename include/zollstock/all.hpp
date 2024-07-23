fb52b6a304d59d0c416f9c2a7eb646e35d0662af
martinfehrs <martinfehrs@web.de>
2024-07-23 23:09:11 +0200

    Ergänzung von mass_units.hpp in all.hpp

--- include/zollstock/all.hpp
+++ include/zollstock/all.hpp
@@ -5,6 +5,7 @@
 #include <zollstock/angle_units.hpp>
 #include <zollstock/length_units.hpp>
 #include <zollstock/time_units.hpp>
+#include <zollstock/mass_units.hpp>
 #include <zollstock/area_units.hpp>
 #include <zollstock/volume_units.hpp>
 
