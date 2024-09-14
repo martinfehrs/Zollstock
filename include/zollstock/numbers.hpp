865e4cbc65836941a0a7b866bb5c8ca70377699d
martinfehrs <martinfehrs@web.de>
2024-09-14 23:41:57 +0200

    Korrektur der pi-Konstandendefinition

--- include/zollstock/numbers.hpp
+++ include/zollstock/numbers.hpp
@@ -13,9 +13,9 @@ namespace zollstock::inline numbers
     template <number_c Number>
     constexpr quantity<units::rad, Number> pi_rad_v{ std::numbers::pi_v<Number> };
 
-    inline constexpr quantity<units::rad, float      > pi_f_rad{ pi_v<float      > };
-    inline constexpr quantity<units::rad, double     > pi_rad  { pi_v<double     > };
-    inline constexpr quantity<units::rad, long double> pi_l_rad{ pi_v<long double> };
+    inline constexpr quantity<units::rad, float      > pi_f_rad{ pi_rad_v<float      > };
+    inline constexpr quantity<units::rad, double     > pi_rad  { pi_rad_v<double     > };
+    inline constexpr quantity<units::rad, long double> pi_l_rad{ pi_rad_v<long double> };
 
 }
 
