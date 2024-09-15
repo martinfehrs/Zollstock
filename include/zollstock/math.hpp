dc12afcdbe52774bf4da4362a47b391f9771919d
martinfehrs <martinfehrs@web.de>
2024-09-15 17:14:41 +0200

    constexpr für mathematische Funktionen

--- include/zollstock/math.hpp
+++ include/zollstock/math.hpp
@@ -7,49 +7,63 @@
 #include <zollstock/unit_one.hpp>
 
 #include <cmath>
+#include <version>
+
+
+#if __cpp_lib_constexpr_cmath >= 202202L
+    #define ZOLLSTOCK_CONSTEXPR_MATH_23 constexpr
+#else
+    #define ZOLLSTOCK_CONSTEXPR_MATH_23
+#endif
+
+#if __cpp_lib_constexpr_cmath >= 202306L
+    #define ZOLLSTOCK_CONSTEXPR_MATH_26 constexpr
+#else
+    #define ZOLLSTOCK_CONSTEXPR_MATH_26
+#endif
 
 
 namespace zollstock
 {
 
     template <number_c Number>
-    [[nodiscard]] auto sin(quantity<units::rad, Number> angle)
+    [[nodiscard]] ZOLLSTOCK_CONSTEXPR_MATH_26 auto sin(quantity<units::rad, Number> angle)
     {
         return quantity<units::_1, Number>{ std::sin(angle.value()) };
     }
 
     template <number_c Number>
-    [[nodiscard]] auto cos(quantity<units::rad, Number> angle)
+    [[nodiscard]] ZOLLSTOCK_CONSTEXPR_MATH_26 auto cos(quantity<units::rad, Number> angle)
     {
         return quantity<units::_1, Number>{ std::cos(angle.value()) };
     }
 
     template <number_c Number>
-    [[nodiscard]] auto tan(quantity<units::rad, Number> angle)
+    [[nodiscard]] ZOLLSTOCK_CONSTEXPR_MATH_26 auto tan(quantity<units::rad, Number> angle)
     {
         return quantity<units::_1, Number>{ std::tan(angle.value()) };
     }
 
     template <number_c Number>
-    [[nodiscard]] auto asin(quantity<units::_1, Number> num)
+    [[nodiscard]] ZOLLSTOCK_CONSTEXPR_MATH_26 auto asin(quantity<units::_1, Number> num)
     {
         return quantity<units::rad, Number>{ std::asin(num.value()) };
     }
 
     template <number_c Number>
-    [[nodiscard]] auto acos(quantity<units::_1, Number> num)
+    [[nodiscard]] ZOLLSTOCK_CONSTEXPR_MATH_26 auto acos(quantity<units::_1, Number> num)
     {
         return quantity<units::rad, Number>{ std::acos(num.value()) };
     }
 
     template <number_c Number>
-    [[nodiscard]] auto atan(quantity<units::_1, Number> num)
+    [[nodiscard]] ZOLLSTOCK_CONSTEXPR_MATH_26 auto atan(quantity<units::_1, Number> num)
     {
         return quantity<units::rad, Number>{ std::atan(num.value()) };
     }
 
     template <number_c Number, unit_c auto unit>
-    [[nodiscard]] bool signbit(quantity<unit, Number> num)
+    [[nodiscard]] ZOLLSTOCK_CONSTEXPR_MATH_23 bool signbit(quantity<unit, Number> num)
     {
         return std::signbit(num.value());
     }
