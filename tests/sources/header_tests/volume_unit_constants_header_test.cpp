f47e4442e2822395436b725e1962e3a3219ebe87
martinfehrs <martinfehrs@web.de>
2024-11-07 23:15:46 +0100

    Test für Volumeneinheitenkonstanten-Header

---
+++ tests/sources/header_tests/volume_unit_constants_header_test.cpp
@@ -0,0 +1,12 @@
+#include <zollstock/volume_unit_constants.hpp>
+#include <zollstock/tests/header_info.hpp>
+
+#include <catch2/catch_all.hpp>
+
+
+TEST_CASE("volume-unit-constants-header", "[headers][volume-units]")
+{
+
+    STATIC_REQUIRE(includes_volume_unit_constants());
+
+}
