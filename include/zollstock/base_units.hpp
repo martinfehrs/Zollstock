79fa72f83a6e39a9fad68f1918f228d2f3dc702c
martinfehrs <martinfehrs@web.de>
2024-11-08 21:18:10 +0100

    Ergänzung eines fehlenden Headers

--- include/zollstock/base_units.hpp
+++ include/zollstock/base_units.hpp
@@ -7,6 +7,7 @@
 #include <zollstock/time_units.hpp>
 #include <zollstock/mass_units.hpp>
 #include <zollstock/temperature_units.hpp>
+#include <zollstock/amount_of_substance_units.hpp>
 
 
 #endif //__ZOLLSTOCK_BASE_UNITS_HPP__
