c549746907094ee21e97721bad20bba4dbf1ac3f
martinfehrs <martinfehrs@web.de>
2024-12-15 00:01:08 +0100

    Ergänzen eines Universal-Headers für den gesamten Header-Baum

---
+++ include/zollstock.hpp
@@ -0,0 +1,14 @@
+#ifndef __ZOLLSTOCK_HPP__
+#define __ZOLLSTOCK_HPP__
+
+
+#include <zollstock/dimensions.hpp>
+#include <zollstock/numbers.hpp>
+#include <zollstock/prefixes.hpp>
+#include <zollstock/quantities.hpp>
+#include <zollstock/static_string.hpp>
+#include <zollstock/tuple_utils.hpp>
+#include <zollstock/units/units.hpp>
+
+
+#endif //__ZOLLSTOCK_HPP__
