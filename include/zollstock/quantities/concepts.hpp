49b773afa8744dfa99183d9bb7d064324fd3bf12
martinfehrs <martinfehrs@web.de>
2025-04-11 20:47:27 +0200

    Entfernung auskommentierter Includes

--- include/zollstock/quantities/concepts.hpp
+++ include/zollstock/quantities/concepts.hpp
@@ -2,20 +2,6 @@
 #define __ZOLLSTOCK_QUANTITIES_CONCEPTS_HPP__
 
 
-//#include <zollstock/quantities/concepts/dimensionless.hpp>
-//
-//#include <zollstock/quantities/concepts/length.hpp>
-//#include <zollstock/quantities/concepts/mass.hpp>
-//#include <zollstock/quantities/concepts/time.hpp>
-//#include <zollstock/quantities/concepts/temperature.hpp>
-//#include <zollstock/quantities/concepts/amount_of_substance.hpp>
-//#include <zollstock/quantities/concepts/electric_current.hpp>
-//
-//#include <zollstock/quantities/concepts/planar_angle.hpp>
-//#include <zollstock/quantities/concepts/area.hpp>
-//#include <zollstock/quantities/concepts/volume.hpp>
-
-
 #include <zollstock/dimensions.hpp>
 #include <zollstock/numbers.hpp>
 #include <zollstock/units/concepts.hpp>
